e32b9c7707e7ae8d534580a2c20db7f35926039e
InkyunMoon <ik.moon23@gmail.com>
2020-11-07 09:06:15 +0900

    5일차 오전 백업

---
+++ my_arth.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    return a + b
+
+def div(a,b):
+    return a/b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def main():
+    print(add(3,4))
+    print(sub(3,4))
+    
+# main()
+# print(__name__)
+
+ㅑㄹ __name__ == '__main__':
+    main()
---
+++ sys1.py
@@ -0,0 +1,12 @@
+import sys
+
+print(sys.argv)
+
+fr = open(sys.argv[1], 'r')
+fw = open(sys.argv[2], 'w')
+
+for line in fr:
+    fw.write(line)
+    
+fr.close()
+fw.close()
